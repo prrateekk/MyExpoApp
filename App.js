0000aa9da0eb4b1f9d691ee0bbb129b5a88b33e6
Prateek Agarwal <prateeag@amazon.com>
2020-12-17 00:24:40 +0530

    Adding a GET API call

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { API, Amplify } from 'aws-amplify'
+import config from './src/aws-exports'
+
+Amplify.configure(config)
+
+class MessageResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      getCalled: false,
+      callSucceeded: false
+    }
+  }
+
+  makeGetCall() {
+    console.log("pressed");
+    this.setState({
+      getCalled: true
+    });
+    API.get('user', '/user')
+    .then(response => {
+      console.log("Got response");
+      console.log(response);
+      this.setState({
+        callSucceeded: true
+      });
+    }).catch(error => {
+      console.log("Got error");
+      console.log(error);
+      this.setState({
+        callSucceeded: false
+      });
+    })
+  }
+
+  getDisplayText() {
+    if (this.state.getCalled === false) {
+      return "Please click the Button";
+    } else {
+      if (this.state.callSucceeded === true) return "Call Succeeded";
+      else return "Call Failed";
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Button title='Press me' onPress={() => this.makeGetCall()} />
+        <Text>{this.getDisplayText()}</Text>
+      </>
+    );
+  }
+}
 
 export default function App() {
+  const abc = "hello bb"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!?</Text>
       <StatusBar style="auto" />
+      <MessageResult/>
     </View>
   );
 }
